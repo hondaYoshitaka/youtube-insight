4d8fe7cbf448269a9e3d6a4e56f6e187a1fb800c
hondaYoshitaka <yhonda1001@gmail.com>
2023-09-14 00:10:36 +0900

    :lock: 特定のパスにのみbasic認証を導入した

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,16 +2,19 @@ import type {Handle} from "@sveltejs/kit";
 import {BASIC_AUTH} from "$env/static/private";
 
 export async function handle({ event, resolve, }: Parameters<Handle>[0]): Promise<ReturnType<Handle>> {
-    const auth = event.request.headers.get("Authorization");
+    const url = new URL(event.request.url);
 
-    if (auth !== `Basic ${btoa(BASIC_AUTH)}`) {
-        return new Response("Not authorized", {
-            status: 401,
-            headers: {
-                "WWW-Authenticate":
-                    'Basic realm="User Visible Realm", charset="UTF-8"',
-            },
-        });
+    if (url.pathname.startsWith("/api")) {
+        const auth = event.request.headers.get("Authorization");
+        if (auth !== `Basic ${btoa(BASIC_AUTH)}`) {
+            return new Response("Not authorized", {
+                status: 401,
+                headers: {
+                    "WWW-Authenticate":
+                        'Basic realm="User Visible Realm", charset="UTF-8"',
+                },
+            });
+        }
     }
     return resolve(event);
 }
