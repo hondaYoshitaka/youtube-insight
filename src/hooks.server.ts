173c415725384295b3b285fd82e76480020ffb75
hondaYoshitaka <yhonda1001@gmail.com>
2023-09-14 00:02:17 +0900

    :lock: basic認証を導入した

---
+++ src/hooks.server.ts
@@ -0,0 +1,17 @@
+import type {Handle} from "@sveltejs/kit";
+import {BASIC_AUTH} from "$env/static/private";
+
+export async function handle({ event, resolve, }: Parameters<Handle>[0]): Promise<ReturnType<Handle>> {
+    const auth = event.request.headers.get("Authorization");
+
+    if (auth !== `Basic ${btoa(BASIC_AUTH)}`) {
+        return new Response("Not authorized", {
+            status: 401,
+            headers: {
+                "WWW-Authenticate":
+                    'Basic realm="User Visible Realm", charset="UTF-8"',
+            },
+        });
+    }
+    return resolve(event);
+}
